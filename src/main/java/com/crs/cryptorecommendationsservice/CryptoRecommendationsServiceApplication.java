665c545472c173c3c13eaccb677bb8c35022d853
Maksym Pavlenko <maksym.pavlenko.ua@gmail.com>
2022-11-05 15:00:15 +0200

    Initial commit

---
+++ src/main/java/com/crs/cryptorecommendationsservice/CryptoRecommendationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crs.cryptorecommendationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRecommendationsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoRecommendationsServiceApplication.class, args);
+    }
+
+}
